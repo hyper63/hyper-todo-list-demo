0f5ce4fa169c3e2200174204ed378e147bdc52e6
Tyler Hall <tyler@hyper.io>
2022-02-10 15:51:46 +0000

    fix: remove path rewrite and mount all backend on /api

--- server/index.js
+++ server/index.js
@@ -11,11 +11,11 @@ app.use(express.static(path.resolve(__dirname, '../build')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/todos', todoRouter);
+app.use('/api/todos', todoRouter);
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({ message: 'Hello World' })
-}) 
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,10 +5,7 @@ module.exports = function(app) {
     '/api',
     createProxyMiddleware({
       target: process.env.SERVER_HOST || 'http://localhost:3010', // SERVER_HOST set in gitpod.yml for Gitpod development
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': '/', // remove base path
-      }
+      changeOrigin: true
     })
   );
 };
