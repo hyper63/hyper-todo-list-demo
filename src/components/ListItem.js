9c7f331f6632db62fe83d6778034b306b0a83fb8
Antonio Dinkins <afdinki@gmail.com>
2022-02-22 17:25:02 +0000

    mFix: removed connections to EditView #25

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,34 +1,17 @@
 import React from 'react'
 // import EditView from './EditView'
+import { Link } from 'react-router-dom'
 
 /*
   component: ListItem
   purpose:   ListItem will display each individual task item in li
-  props:     editTodo, deleteTodo, id, task, completed (received from TodoList)
+  props:     deleteTodo, id, task, completed (received from TodoList)
   children:  EditView
-  methods:   onClickHandler, remove
-             onClickHandler - used to open EditView
-                            - provides EditView with the following props:
-                                - update    ---> editTodo
-                                - id        ---> id
-                                - task      ---> task
-                                - completed ---> completed
-            remove - deletes a task given its id
+
 */
 
 const ListItem = (props) => {
-  const { editTodo, deleteTodo, id, task, completed } = props
-
-  const onClickHandler = ({ id, task, completed, editTodo, deleteTodo }) => {
-    // open EditView with todo
-    // pass down editTodo, id, task, completed to SingleTodoView
-    /* <EditView update={editTodo}
-                id={id}
-                task={task}
-                completed={completed}
-    />
-     */
-  }
+  const { id, task, deleteTodo } = props
 
   async function remove (id) {
     try {
@@ -38,12 +21,15 @@ const ListItem = (props) => {
     }
   }
   return (
-    <li onClick={() => onClickHandler({ id, task, completed, editTodo })}>
-      <div>{task}</div>
-      <div>
-        <button type='button' name='delete' onClick={() => remove(id)}>Delete Task</button>
-      </div>
-    </li>
+    <Link to={`/todos/${id}`}>
+      <li>
+        <div>{task}</div>
+        <div>
+          <button type='button' name='delete' onClick={() => remove(id)}>Delete Task</button>
+        </div>
+      </li>
+    </Link>
+
   )
 }
 export default ListItem
