2c00926d60577e31cb313bb5f9ba9cd40e331cc5
Antonio Dinkins <afdinki@gmail.com>
2022-03-01 19:05:28 +0000

    Fix: added error handling to getSingleTodo route #40

--- server/routes/getSingleTodo.js
+++ server/routes/getSingleTodo.js
@@ -3,6 +3,14 @@ const { connect } = require('hyper-connect')
 const hyper = connect(process.env.HYPER)
 
 module.exports = async function getSingleTodo (req, res) {
-  const result = await hyper.data.get(req.params.id)
-  res.json(result)
+  try {
+    const result = await hyper.data.get(req.params.id)
+    if (result) {
+      res.json(result)
+    } else {
+      res.status(404).json({ message: 'Task not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Unable to access information', error })
+  }
 }
