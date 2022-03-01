17572a94077284a157e29c621adce984829d9602
Antonio Dinkins <afdinki@gmail.com>
2022-03-01 19:26:04 +0000

    Fix: added error checking to delete route #41

--- server/routes/deleteTodo.js
+++ server/routes/deleteTodo.js
@@ -3,6 +3,14 @@ const { connect } = require('hyper-connect')
 const hyper = connect(process.env.HYPER)
 
 module.exports = async function deleteTodos (req, res) {
+  try {
+    const todo = await hyper.data.get(req.params.id)
+    if (!todo) {
+      res.status(404).json({ message: 'Task not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Unable to delete task', error })
+  }
   const result = await hyper.data.remove(req.params.id)
   res.json(result)
 }
--- server/routes/getAllTodos.js
+++ server/routes/getAllTodos.js
@@ -3,6 +3,10 @@ const { connect } = require('hyper-connect')
 const hyper = connect(process.env.HYPER)
 
 module.exports = async function getAllTodos (req, res) {
-  const result = await hyper.data.list()
-  res.json(result)
+  try {
+    const result = await hyper.data.list()
+    res.json(result)
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong', error })
+  }
 }
