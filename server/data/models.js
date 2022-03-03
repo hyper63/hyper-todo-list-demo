90ca82348bbcd58346936c748cff182fd9de138f
Antonio Dinkins <afdinki@gmail.com>
2022-03-03 15:19:36 +0000

    Feature: added todoSchema as Todo in createTodos #47

--- server/data/models.js
+++ server/data/models.js
@@ -1,9 +1,9 @@
-import { z } from 'zod'
+const { z } = require('zod')
 
 const todoSchema = z.object({
   id: z.string(),
   task: z.string(),
   completed: z.boolean()
 })
-
-export default todoSchema
+// type Todo = z.infer<typeof todoSchema>
+module.exports = todoSchema
--- server/routes/createTodos.js
+++ server/routes/createTodos.js
@@ -2,6 +2,7 @@ const { connect } = require('hyper-connect')
 const { nanoid } = require('nanoid')
 
 const hyper = connect(process.env.HYPER)
+const Todo = require('../data/models')
 
 module.exports = async function createTodos (req, res) {
   console.log(req.body)
@@ -10,8 +11,10 @@ module.exports = async function createTodos (req, res) {
       res.status(400).json({ message: 'Add a new task' })
     } else {
       const newTodo = { id: nanoid(), task: req.body.task, completed: req.body.completed }
-      const result = await hyper.data.add(newTodo)
-      res.json(result)
+      if (Todo.parse({ task: newTodo.task })) {
+        const result = await hyper.data.add(newTodo)
+        res.json(result)
+      }
     }
   } catch (error) {
     res.status(500).json({ message: 'Unable to create a new task', error })
