49fedf5d0805baac47de1e8c25e0cd7e62966dcd
Antonio Dinkins <afdinki@gmail.com>
2022-03-09 14:46:02 +0000

    Fix: moved response inside the try catch block in deleteTodo #53

--- server/routes/deleteTodo.js
+++ server/routes/deleteTodo.js
@@ -7,10 +7,11 @@ module.exports = async function deleteTodos (req, res) {
     const todo = await hyper.data.get(req.params.id)
     if (!todo) {
       res.status(404).json({ message: 'Task not found' })
+    } else {
+      const result = await hyper.data.remove(req.params.id)
+      res.json(result)
     }
   } catch (error) {
     res.status(500).json({ message: 'Unable to delete task', error })
   }
-  const result = await hyper.data.remove(req.params.id)
-  res.json(result)
 }
