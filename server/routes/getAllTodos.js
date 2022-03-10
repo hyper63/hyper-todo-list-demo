6711a2ef64279d7c2ebddca22ef64fd9ee8383f7
Antonio Dinkins <afdinki@gmail.com>
2022-03-10 16:35:13 +0000

    Enhancement: modified getAllTodos to query items for type todo instead of filtering #54

--- server/routes/getAllTodos.js
+++ server/routes/getAllTodos.js
@@ -4,9 +4,10 @@ const hyper = connect(process.env.HYPER)
 
 module.exports = async function getAllTodos (req, res) {
   try {
-    const { docs } = await hyper.data.list()
-    const todos = docs.filter(item => item.type === 'todo')
-    res.json(todos)
+    // const { docs } = await hyper.data.list()
+    // const todos = docs.filter(item => item.type === 'todo')
+    const { docs } = await hyper.data.query({ type: 'todo' })
+    res.json(docs)
   } catch (error) {
     res.status(500).json({ message: 'Something went wrong', error })
   }
