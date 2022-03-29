60a85b9b387ca5e0eb6060c01fa6d0146802e13b
Antonio Dinkins <afdinki@gmail.com>
2022-03-29 20:15:05 +0000

    Enhancement: added check based on condition of completed in EditView #85

--- src/components/EditView.js
+++ src/components/EditView.js
@@ -26,7 +26,7 @@ import React, { useState } from 'react'
  */
 
 const EditView = (props) => {
-  const { update, id, task } = props
+  const { update, id, task, completed } = props
   const [todo, setTodo] = useState('')
   const [done, setDone] = useState(false)
   console.log('Id: ', id)
@@ -53,7 +53,13 @@ const EditView = (props) => {
       <label htmlFor='task' />
       <input placeholder={task} value={todo} onChange={handleChange} name='task' />
       <label htmlFor='completed'>Task Completed: </label>
-      <input type='checkbox' name='completed' onChange={handleChecked} />
+      <input
+        type='checkbox'
+        name='completed'
+        onChange={handleChecked}
+        value={done}
+        checked={completed}
+      />
       <button>Save</button>
 
     </form>
