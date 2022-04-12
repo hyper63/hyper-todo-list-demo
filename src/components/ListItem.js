4b7dcf2032cda1ec2cd9345666a495d89be414c8
Antonio Dinkins <afdinki@gmail.com>
2022-04-12 22:08:33 +0000

    Added chakra elements to ListItem.js

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import EditView from './EditView'
 import { Link } from 'react-router-dom'
-import { ListItem } from '@chakra-ui/react'
+import { ListItem, Flex, Box, Button } from '@chakra-ui/react'
 
 /*
   component: TaskItem
@@ -15,10 +15,12 @@ const TaskItem = (props) => {
   const { _id, task, remove, completed } = props
   return (
     <ListItem className={completed ? 'completed' : ''}>
-      <div><Link to={`/todos/${_id}`}>{task}</Link></div>
-      <div>
-        <button type='button' name='delete' onClick={() => remove(_id)}>Delete</button>
-      </div>
+      <Flex m={5} p={4}>
+        <Box marginRight={5}><Link to={`/todos/${_id}`}>{task}</Link></Box>
+        <Box>
+          <Button type='button' name='delete' onClick={() => remove(_id)}>Delete</Button>
+        </Box>
+      </Flex>
     </ListItem>
 
   )
